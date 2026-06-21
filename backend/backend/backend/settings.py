13ef0e2889b2a1bab888abebb2f9690866e792e7
Divya Jain <divyajain112002@gmail.com>
2026-06-21 14:22:24 +0530

    allowed hosts config updated

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)v!w(pds-chm(t96=_)f2^3m-z@o($1c9ul@z5@27rcdvkk-6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
